ab8db9053015ec5ccb26f63e3fba6874ae1e9a31
blaze <blaze.vincent@hotmail.com>
2023-06-05 01:57:21 -0500

    Add method for spherical deformation of plane

--- game/src/main/java/dev/n1t/game/mesh/TerrainMesh.java
+++ game/src/main/java/dev/n1t/game/mesh/TerrainMesh.java
@@ -5,22 +5,32 @@ import com.jme3.scene.Mesh;
 import com.jme3.scene.VertexBuffer;
 import com.jme3.util.BufferUtils;
 
+import java.util.Arrays;
+
 public class TerrainMesh extends Mesh {
+
+    private final int squareResolution;
+    private final Vector3f[] vertices;
+
     public TerrainMesh(int squareSideLength, int squareResolution){
         super();
 
-        Vector3f[] vertices = new Vector3f[(int) Math.pow(squareResolution, 2)];
+        this.squareResolution = squareResolution;
+
+        //generate vertices
+        this.vertices = new Vector3f[(int) Math.pow(squareResolution, 2)];
 
         for(int x = 0; x < squareResolution; x++){
             for(int y = 0; y < squareResolution; y++){
                 vertices[x * squareResolution + y] = new Vector3f(
                     squareSideLength * (float) x / (float) (squareResolution - 1),
                     squareSideLength * (float) y / (float) (squareResolution - 1),
-                    0f
+                    1f
                 );
             }
         }
 
+        //generate faces
         final int squareFaceResolution = squareResolution - 1;
         int[] indexes = new int[6 * (int) Math.pow(squareFaceResolution, 2)];
         int[] facePointOffsets = {1, 0, squareResolution, squareResolution, squareResolution + 1, 1};
@@ -36,6 +46,33 @@ public class TerrainMesh extends Mesh {
 
         this.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
         this.setBuffer(VertexBuffer.Type.Index,    3, BufferUtils.createIntBuffer(indexes));
+        this.updateBound();
+    }
+
+    public void sphericalDepress(int radius, int centerLocationIndex, float centerDepth){
+
+        if(centerLocationIndex > vertices.length || centerLocationIndex < 0)
+            throw new RuntimeException("Diamond depression center index out of vertex buffer bounds");
+
+        if(radius <= 0)
+            throw new RuntimeException("Radius must be positive");
+
+        for (int x = 0; x < squareResolution; x++) {
+            for (int y = 0; y < squareResolution; y++) {
+                int index = x * squareResolution + y;
+
+                final int xDistFromCenter = x - centerLocationIndex % squareResolution;
+                final int yDistFromCenter = y - centerLocationIndex / squareResolution;
+                final float distancesSquaredSum = (float) (Math.pow(yDistFromCenter, 2) + Math.pow(xDistFromCenter, 2));
+
+                if (distancesSquaredSum <= Math.pow(radius, 2)) {
+                    float depthFactor = 1.0f - (distancesSquaredSum / (float) (Math.pow(radius, 2)));
+                    vertices[index].setZ(vertices[index].getZ() - centerDepth * depthFactor);
+                }
+            }
+        }
+
+        this.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
         this.updateBound();
     }
 }
